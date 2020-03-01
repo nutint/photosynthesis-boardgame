076e6566cab4f85d0a3ff748bd2e0fcdc85bd716
Natthapong Intharak <nattha.int@gmail.com>
2020-03-01 22:00:50 +0700

    Simple scoring with 1 item on the board

--- src/test/scala/com/nat/photosynthesis/model/ScoreEngineSpec.scala
+++ src/test/scala/com/nat/photosynthesis/model/ScoreEngineSpec.scala
@@ -12,9 +12,24 @@ class ScoreEngineSpec extends FreeSpec with Matchers {
       ScoreEngine.calculatePlayerScore(john, forestBlocks, SunLocation0) shouldBe 0
     }
 
+    "should return 0 if there is 1 seed in the board and its the same color" in {
+      val forestBlock = List(ForestBlock(BoardLocation(0, 3, 3), Seed(Green)))
+      ScoreEngine.calculatePlayerScore(john, forestBlock, SunLocation0) shouldBe 0
+    }
+
     "should return 1 if there is 1 small tree in the board and its the same color" in {
       val forestBlock = List(ForestBlock(BoardLocation(0, 3, 3), SmallTree(Green)))
       ScoreEngine.calculatePlayerScore(john, forestBlock, SunLocation0) shouldBe 1
+    }
+
+    "should return 2 if there is 1 medium tree in the board and its the same color" in {
+      val forestBlock = List(ForestBlock(BoardLocation(0, 3, 3), MediumTree(Green)))
+      ScoreEngine.calculatePlayerScore(john, forestBlock, SunLocation0) shouldBe 2
+    }
+
+    "should return 3 if there is 1 large tree in the board and its the same color" in {
+      val forestBlock = List(ForestBlock(BoardLocation(0, 3, 3), LargeTree(Green)))
+      ScoreEngine.calculatePlayerScore(john, forestBlock, SunLocation0) shouldBe 3
     }
   }
 }
