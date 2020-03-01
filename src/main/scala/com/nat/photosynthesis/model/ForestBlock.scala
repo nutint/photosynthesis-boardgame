c39a0cc459fd4bfbf1d3dca67954564a6cbd27ca
Natthapong Intharak <nattha.int@gmail.com>
2020-03-01 21:57:32 +0700

    Scoring Engine initial

--- src/main/scala/com/nat/photosynthesis/model/ForestBlock.scala
+++ src/main/scala/com/nat/photosynthesis/model/ForestBlock.scala
@@ -3,4 +3,9 @@ package com.nat.photosynthesis.model
 case class ForestBlock(
   boardLocation: BoardLocation,
   plantItem: PlantItem
-)
+) {
+  def calculateScore(sunLocation: SunLocation, forestBlocks: List[ForestBlock]): Int = {
+    if(forestBlocks.length == 1 && forestBlocks.contains(this)) plantItem.score
+    else 0
+  }
+}
--- src/main/scala/com/nat/photosynthesis/model/PlantItem.scala
+++ src/main/scala/com/nat/photosynthesis/model/PlantItem.scala
@@ -1,7 +1,18 @@
 package com.nat.photosynthesis.model
 
-sealed trait PlantItem
-case class Seed(plantType: PlantType) extends PlantItem
-case class SmallTree(plantType: PlantType) extends PlantItem
-case class MediumTree(plantType: PlantType) extends PlantItem
-case class LargeTree(plantType: PlantType) extends PlantItem
+sealed trait PlantItem {
+  def plantType: PlantType
+  def score: Int
+}
+case class Seed(plantType: PlantType) extends PlantItem {
+  override def score: Int = 0
+}
+case class SmallTree(plantType: PlantType) extends PlantItem {
+  override def score: Int = 1
+}
+case class MediumTree(plantType: PlantType) extends PlantItem {
+  override def score: Int = 2
+}
+case class LargeTree(plantType: PlantType) extends PlantItem {
+  override def score: Int = 3
+}
