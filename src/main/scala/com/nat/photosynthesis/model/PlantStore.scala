e037b0dd354f74afaf7622df23c8e5ed8ff06853
Natthapong Intharak <nattha.int@gmail.com>
2020-03-07 12:30:01 +0700

    putBack, take, getPrice

--- src/main/scala/com/nat/photosynthesis/model/PlantStore.scala
+++ src/main/scala/com/nat/photosynthesis/model/PlantStore.scala
@@ -6,7 +6,38 @@ case class PlantStore(
   smallTreeStore: StoreSpace[SmallTree],
   mediumTreeStore: StoreSpace[MediumTree],
   largeTreeStore: StoreSpace[LargeTree]
-)
+) {
+  def getPrice(plantItem: PlantItem): Either[String, Int] =
+    plantItem match {
+      case _: Seed => seedStore.currentPrice
+      case _: SmallTree => smallTreeStore.currentPrice
+      case _: MediumTree => mediumTreeStore.currentPrice
+      case _: LargeTree => largeTreeStore.currentPrice
+    }
+
+  def take(plantItem: PlantItem): Either[String, PlantStore] =
+    plantItem match {
+      case _: Seed => seedStore.take.map(ss => copy(seedStore = ss))
+      case _: SmallTree => smallTreeStore.take.map(ss => copy(smallTreeStore = ss))
+      case _: MediumTree => mediumTreeStore.take.map(ss => copy(mediumTreeStore = ss))
+      case _: LargeTree => largeTreeStore.take.map(ss => copy(largeTreeStore = ss))
+    }
+
+  def putBack(plantItem: PlantItem):Either[String, PlantStore] =
+    plantItem match {
+      case _: Seed => seedStore.putBack.map(ss => copy(seedStore = ss))
+      case _: SmallTree => smallTreeStore.putBack.map(ss => copy(smallTreeStore = ss))
+      case _: MediumTree => mediumTreeStore.putBack.map(ss => copy(mediumTreeStore = ss))
+      case _: LargeTree => largeTreeStore.putBack.map(ss => copy(largeTreeStore = ss))
+    }
+
+  def calculate[A](plantItem: PlantItem, fn: StoreSpace[_] => Either[String, A]): Either[String, A] = plantItem match {
+    case _: Seed => fn(seedStore)
+    case _: SmallTree => fn(smallTreeStore)
+    case _: MediumTree => fn(mediumTreeStore)
+    case _: LargeTree => fn(largeTreeStore)
+  }
+}
 
 object PlantStore {
 
--- src/test/scala/com/nat/photosynthesis/model/PlantStoreSpec.scala
+++ src/test/scala/com/nat/photosynthesis/model/PlantStoreSpec.scala
@@ -4,15 +4,48 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class PlantStoreSpec extends FreeSpec with Matchers {
 
+  private val seedStoreSpace: StoreSpace[Seed] = StoreSpace[Seed](List(1, 1, 2, 2))
+  private val smallTreeStoreSpace: StoreSpace[SmallTree] = StoreSpace[SmallTree](List(2, 2, 3, 3))
+  private val mediumTreeStoreSpace: StoreSpace[MediumTree] = StoreSpace[MediumTree](List(3, 3, 4, 4))
+  private val largeTreeStoreSpace: StoreSpace[LargeTree] = StoreSpace[LargeTree](List(4, 5))
+  private val originalPlantStore = PlantStore(
+    plantType = Green,
+    seedStore = seedStoreSpace,
+    smallTreeStore = smallTreeStoreSpace,
+    mediumTreeStore = mediumTreeStoreSpace,
+    largeTreeStore = largeTreeStoreSpace
+  )
+
   "PlantStore.apply" - {
     "should create correctly" in {
-      PlantStore(Green) shouldBe PlantStore(
-        plantType = Green,
-        seedStore = StoreSpace[Seed](List(1, 1, 2, 2)),
-        smallTreeStore = StoreSpace[SmallTree](List(2, 2, 3, 3)),
-        mediumTreeStore = StoreSpace[MediumTree](List(3, 3, 4, 4)),
-        largeTreeStore = StoreSpace[LargeTree](List(4, 5))
-      )
+      PlantStore(Green) shouldBe originalPlantStore
+    }
+  }
+
+  "getPrice" - {
+    "should work correctly" in {
+      originalPlantStore.getPrice(Seed(Green)) shouldBe seedStoreSpace.currentPrice
+      originalPlantStore.getPrice(SmallTree(Green)) shouldBe smallTreeStoreSpace.currentPrice
+      originalPlantStore.getPrice(MediumTree(Green)) shouldBe mediumTreeStoreSpace.currentPrice
+      originalPlantStore.getPrice(LargeTree(Green)) shouldBe largeTreeStoreSpace.currentPrice
+    }
+  }
+
+  "take" - {
+    "should work correctly" in {
+      originalPlantStore.take(Seed(Green)) shouldBe seedStoreSpace.take.map(ss => originalPlantStore.copy(seedStore = ss))
+      originalPlantStore.take(SmallTree(Green)) shouldBe smallTreeStoreSpace.take.map(ss => originalPlantStore.copy(smallTreeStore = ss))
+      originalPlantStore.take(MediumTree(Green)) shouldBe mediumTreeStoreSpace.take.map(ss => originalPlantStore.copy(mediumTreeStore = ss))
+      originalPlantStore.take(LargeTree(Green)) shouldBe largeTreeStoreSpace.take.map(ss => originalPlantStore.copy(largeTreeStore = ss))
+    }
+  }
+
+  "putBack" - {
+    "should work correctly" in {
+      originalPlantStore.putBack(Seed(Green)) shouldBe seedStoreSpace.putBack.map(ss => originalPlantStore.copy(seedStore = ss))
+      originalPlantStore.putBack(SmallTree(Green)) shouldBe smallTreeStoreSpace.putBack.map(ss => originalPlantStore.copy(smallTreeStore = ss))
+      originalPlantStore.putBack(MediumTree(Green)) shouldBe mediumTreeStoreSpace.putBack.map(ss => originalPlantStore.copy(mediumTreeStore = ss))
+      originalPlantStore.putBack(LargeTree(Green)) shouldBe largeTreeStoreSpace.putBack.map(ss => originalPlantStore.copy(largeTreeStore = ss))
     }
   }
 }
