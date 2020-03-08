8dbd1e8873e2b236c80f1855a984b05b760da182
Natthapong Intharak <nattha.int@gmail.com>
2020-03-08 23:28:40 +0700

    serialize BoardLocationTier

--- src/main/scala/com/nat/photosynthesis/controller/JsonFormats.scala
+++ src/main/scala/com/nat/photosynthesis/controller/JsonFormats.scala
@@ -33,6 +33,26 @@ object JsonFormats {
     }
   }
 
+  implicit object BoardLocationTierFormat extends JsonFormat[BoardLocationTier] {
+    override def read(json: JsValue): BoardLocationTier = json match {
+      case JsString(strVal) => strVal.trim.toLowerCase match {
+        case "tier1" => BoardLocationTier1
+        case "tier2" => BoardLocationTier2
+        case "tier3" => BoardLocationTier3
+        case "tier4" => BoardLocationTier4
+        case _ => deserializationError("invalid board location tier value: expected (tier1, tier2, tier3, tier4)")
+      }
+      case _ => deserializationError("invalid board location tier value: expected string")
+    }
+
+    override def write(obj: BoardLocationTier): JsValue = JsString(obj match {
+      case BoardLocationTier1 => "tier1"
+      case BoardLocationTier2 => "tier2"
+      case BoardLocationTier3 => "tier3"
+      case BoardLocationTier4 => "tier4"
+    })
+  }
+
   implicit val boardLocationFormat = jsonFormat3(BoardLocation.apply)
   implicit val playerFormat = jsonFormat2(Player.apply)
 }
