fc407a061e91932108ecce428b880d57de20a8ed
Natthapong Intharak <nattha.int@gmail.com>
2020-03-08 23:09:28 +0700

    Test refactoring

--- src/test/scala/com/nat/photosynthesis/controller/JsonFormatsSpec.scala
+++ src/test/scala/com/nat/photosynthesis/controller/JsonFormatsSpec.scala
@@ -12,72 +12,76 @@ class JsonFormatsSpec extends FreeSpec with Matchers {
   import spray.json._
 
   "PlantTypeFormat" - {
-    "should be able to decode the following examples" in {
-      val examples =
-        Table(
-          ("string", "expectedPlantType"),
-          ("green", Green),
-          ("Green", Green),
-          ("GrEen ", Green),
-          ("Yellow", Yellow),
-          ("Orange", Orange),
-          ("Blue", Blue)
-        )
-      forAll(examples) { (jsonString, plantType) =>
-        PlantTypeFormat.read(JsString(jsonString)) shouldBe plantType
-      }
-    }
+    "read" - {
 
-    "should error when pass other string" in {
-      Try(PlantTypeFormat.read(JsString("other string"))) match {
-        case Failure(ex) => ex.getMessage shouldBe "invalid plant type value: expected (green, yellow, blue, orange)"
-        case _ => assert(false)
+      "should be able to decode the following examples" in {
+        val examples =
+          Table(
+            ("string", "expectedPlantType"),
+            ("green", Green),
+            ("Green", Green),
+            ("GrEen ", Green),
+            ("Yellow", Yellow),
+            ("Orange", Orange),
+            ("Blue", Blue)
+          )
+        forAll(examples) { (jsonString, plantType) =>
+          PlantTypeFormat.read(JsString(jsonString)) shouldBe plantType
+        }
       }
-    }
 
-    "should error when pass other json kind" in {
-      Try(PlantTypeFormat.read(JsNumber(1))) match {
-        case Failure(ex) => ex.getMessage shouldBe "invalid plant type value: expected string"
-        case _ => assert(false)
+      "should error when pass other string" in {
+        Try(PlantTypeFormat.read(JsString("other string"))) match {
+          case Failure(ex) => ex.getMessage shouldBe "invalid plant type value: expected (green, yellow, blue, orange)"
+          case _ => assert(false)
+        }
+      }
+
+      "should error when pass other json kind" in {
+        verifyReadError[PlantType](JsNumber(1), "invalid plant type value: expected string")
       }
     }
   }
 
   "boardLocationFormat" - {
-    "should return object when decode success" in {
-      val sourceString = """{ "x": 1, "y": 2, "z": 3 }""".parseJson
-      boardLocationFormat.read(sourceString) shouldBe BoardLocation(1, 2, 3)
-    }
-    "should throw error when decode fail" in {
-      val sourceString = """{ "y": 2, "z": 3 }""".parseJson
-      Try(boardLocationFormat.read(sourceString)) match {
-        case Failure(exception) => exception.getMessage shouldBe "Object is missing required member 'x'"
-        case _ => assert(false)
+    "read" - {
+
+      "should return object when decode success" in {
+        val sourceString = """{ "x": 1, "y": 2, "z": 3 }""".parseJson
+        boardLocationFormat.read(sourceString) shouldBe BoardLocation(1, 2, 3)
+      }
+      "should throw error when decode fail" in {
+        val sourceString = """{ "y": 2, "z": 3 }""".parseJson
+        verifyReadError[BoardLocation](sourceString, "Object is missing required member 'x'")
       }
     }
   }
 
   "playerFormat" - {
-    "should decode the following json correctly" in {
-      val sourceJson = """{ "name": "John", "plantType": "green"}""".parseJson
-      playerFormat.read(sourceJson) shouldBe Player("John", Green)
-    }
+    "read" - {
 
-    "should decode fail if input incorrect player field" in {
-      val sourceJson = """{ "namee": "John", "plantType": "green"}""".parseJson
-      Try(playerFormat.read(sourceJson))  match {
-        case Failure(exception) => exception.getMessage shouldBe "Object is missing required member 'name'"
-        case _ => assert(false)
+      "should decode the following json correctly" in {
+        val sourceJson = """{ "name": "John", "plantType": "green"}""".parseJson
+        playerFormat.read(sourceJson) shouldBe Player("John", Green)
+      }
+
+      "should decode fail if input incorrect player field" in {
+        val sourceJson = """{ "namee": "John", "plantType": "green"}""".parseJson
+        verifyReadError[Player](sourceJson, "Object is missing required member 'name'")
+      }
+
+      "should fail if unable to decode plantType" in {
+        val sourceJson = """{ "name": "John", "plantType": "greeeen"}""".parseJson
+        verifyReadError[Player](sourceJson, "invalid plant type value: expected (green, yellow, blue, orange)")
       }
     }
 
-    "should fail if unable to decode plantType" in {
-      val sourceJson = """{ "name": "John", "plantType": "greeeen"}""".parseJson
-      Try(playerFormat.read(sourceJson)) match {
-        case Failure(exception) => exception.getMessage shouldBe "invalid plant type value: expected (green, yellow, blue, orange)"
-        case _ => assert(false)
-      }
-    }
+  }
 
+  private def verifyReadError[A](sourceJson: JsValue, expectedError: String)(implicit jsonFormat: JsonFormat[A]): Any = {
+    Try(jsonFormat.read(sourceJson)) match {
+      case Failure(exception) => exception.getMessage shouldBe expectedError
+      case _ => assert(false)
+    }
   }
 }
