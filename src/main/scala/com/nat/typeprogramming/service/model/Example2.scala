785fb1fb93781d8d955a26a500cde3a420723f3b
Natthapong Intharak <nattha.int@gmail.com>
2020-04-13 14:51:18 +0700

    Add REQ-01

--- src/main/scala/com/nat/typeprogramming/service/model/Example2.scala
+++ src/main/scala/com/nat/typeprogramming/service/model/Example2.scala
@@ -20,7 +20,7 @@ object Example2 {
     sealed trait UserAccount
     case class NonVerifiedAccount(name: String, email: String) extends UserAccount
     case class VerifiedAccount(name: String, email: String) extends UserAccount
-    case class PremiumAccount(name: String, email: String, expireDate: Date) extends UserAccount
+    case class PremiumAccount(name: String, email: String) extends UserAccount
 
   }
 }
