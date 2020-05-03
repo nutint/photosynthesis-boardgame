8a212bb0ac5f82f50afaf8e1d76085c4f78bdc27
Natthapong Intharak <nattha.int@gmail.com>
2020-05-03 17:40:29 +0700

    Fix unable to test with ask pattern, and test stopping actor

--- src/test/scala/com/nat/photosynthesis/experiment/AkkaStreamSpec.scala
+++ src/test/scala/com/nat/photosynthesis/experiment/AkkaStreamSpec.scala
@@ -1,39 +1,72 @@
 package com.nat.photosynthesis.experiment
 
-import scala.concurrent.duration._
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.actor.testkit.typed.scaladsl.ManualTime
-import akka.actor.testkit.typed.scaladsl.TestProbe
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.typed.scaladsl.AskPattern._
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior, Scheduler}
+import akka.util.Timeout
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.Try
+
 class AkkaStreamSpec
-  extends ScalaTestWithActorTestKit(ManualTime.config) with AnyWordSpecLike {
+  extends AnyWordSpecLike
+    with BeforeAndAfterAll
+    with Matchers {
 
-  val manualTime: ManualTime = ManualTime()
+  val testKit = ActorTestKit()
 
-  "A timer" must {
-    "schedule non-repeated ticks" in {
-      case object Tick
-      case object Tock
+  override def afterAll(): Unit = testKit.shutdownTestKit()
 
-      val probe = TestProbe[Tock.type]()
-      val behavior = Behaviors.withTimers[Tick.type] { timer =>
-        timer.startSingleTimer("T", Tick, 10.millis)
-        Behaviors.receiveMessage { _ =>
-          probe.ref ! Tock
-          Behaviors.same
-        }
-      }
+  "Echo" must {
+    import AkkaStreamSpec._
+    "pong if pinger ping" in {
+      val pinger = testKit.spawn(Echo(), "ping")
+      val probe = testKit.createTestProbe[Echo.Pong]()
+      pinger ! Echo.Ping("Hello", probe.ref)
+      probe.expectMessage(Echo.Pong("Hello"))
+    }
 
-      spawn(behavior)
+    "be able to stop actors under test" in {
+      val probe = testKit.createTestProbe[Echo.Pong]()
 
-      manualTime.expectNoMessageFor(9.millis, probe)
+      val pinger1 = testKit.spawn(Echo(), "pinger")
+      pinger1 ! Echo.Ping("Hello", probe.ref)
+      probe.expectMessage(Echo.Pong("Hello"))
+      testKit.stop(pinger1)
 
-      manualTime.timePasses(2.millis)
-      probe.expectMessage(Tock)
+      val pinger2 = testKit.spawn(Echo(), "pinger")
+      pinger2 ! Echo.Ping("Hello", probe.ref)
+      probe.expectMessage(Echo.Pong("Hello"))
+      testKit.stop(pinger2, 10 seconds)
+    }
+  }
+}
 
-      manualTime.expectNoMessageFor(10.seconds, probe)
+object AkkaStreamSpec {
+  object Echo {
+    case class Ping(message: String, response: ActorRef[Pong])
+    case class Pong(message: String)
+
+    def apply(): Behavior[Ping] = Behaviors.receiveMessage {
+      case Ping(msg, replyTo) =>
+        replyTo ! Pong(msg)
+        Behaviors.same
+    }
+  }
+
+  object MockedBehavior {
+    case class Message(i: Int, replyTo: ActorRef[Try[Int]])
+
+    class Producer(publisher: ActorRef[Message])(implicit scheduler: Scheduler) {
+      def produce(messages: Int)(implicit timeout: Timeout): Unit =
+        (0 until messages).foreach(publish)
+      def publish(i: Int)(implicit timeout: Timeout): Future[Try[Int]] =
+        publisher.ask(ref => Message(i, ref))
     }
   }
 }
