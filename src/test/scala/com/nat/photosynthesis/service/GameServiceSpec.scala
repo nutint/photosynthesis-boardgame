0041d02ccb4e67be07e52b9a7774ead155ba8738
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-11-22 19:57:35 +0700

    add test on gameService using UUIDGenerator once

--- src/test/scala/com/nat/photosynthesis/service/GameServiceSpec.scala
+++ src/test/scala/com/nat/photosynthesis/service/GameServiceSpec.scala
@@ -5,42 +5,62 @@ import com.nat.photosynthesis.service.model.engine.{GameEngine, Registration}
 import com.nat.photosynthesis.service.model.{AddPlayer, Green, Player, Processor, ScoringTokenStacks, engine}
 import com.nat.photosynthesis.service.repository.GameRepository
 import com.nat.photosynthesis.utils.UUIDGenerator
-import org.scalatest.{AsyncFreeSpec, FreeSpec, Matchers}
+import org.scalatest.{AsyncFreeSpec, BeforeAndAfterEach, Matchers}
 import org.scalatest.mockito.MockitoSugar
 import org.mockito.Mockito._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class GameServiceSpec
   extends AsyncFreeSpec
     with MockitoSugar
     with Matchers
+    with BeforeAndAfterEach
 {
   import Processor._
 
   val mockedUUIDGenerator = mock[UUIDGenerator]
   private val gameId = "69e304e4-1716-4255-abbf-bbe04a29e7fe"
-  when(mockedUUIDGenerator.generate()).thenReturn(gameId)
+
+  override def beforeEach() = {
+    reset(mockedUUIDGenerator)
+    when(mockedUUIDGenerator.generate()).thenReturn(gameId)
+  }
 
   "createGame" - {
     "should be able to create game when no error" in {
       val initialGameEngine = Registration(Nil, ScoringTokenStacks())
       val john = Player("John", Green)
-      val expectedGameEngine: Either[String, Identifiable[GameEngine]] =
-        initialGameEngine
-          .processCommand(AddPlayer(john))
-          .map(x => Identifiable(gameId, x))
+      val newGameEngineWithNoID = initialGameEngine
+        .processCommand(AddPlayer(john))
 
       val gameService = new GameService(mock[GameRepository], mockedUUIDGenerator)
 
-      // M(x).map(x -> y): M(y)
-      gameService.createGame(john:: Nil).map({
-        case Right(newGameEngine) =>
-          expectedGameEngine
-            .map(x => x shouldEqual newGameEngine)
-            .getOrElse(assert(false))
-        case _ => assert(false)
-      })
+      gameService.createGame(john:: Nil).map(_.map(_.value) shouldEqual newGameEngineWithNoID)
     }
-//    "should be able to add player when the room is empty"
+
+    "should use generated game id from UUIDGenerator" in {
+      val john = Player("John", Green)
+
+      val gameService = new GameService(mock[GameRepository], mockedUUIDGenerator)
+
+      val expectedGameId: Either[String, String] = Right(gameId)
+
+      val newGame = Await.result(gameService.createGame(john:: Nil), 5 second)
+      newGame.map(_.id) shouldEqual expectedGameId
+    }
+
+    "should use UUIDGenerator once" in {
+      val john = Player("John", Green)
+
+      val gameService = new GameService(mock[GameRepository], mockedUUIDGenerator)
+
+      Await.result(gameService.createGame(john:: Nil), 5 second)
+      verify(mockedUUIDGenerator, times(1)) generate() shouldEqual null
+    }
+
+    "violate rules" is pending
   }
 
 }
